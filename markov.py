8776ef20b8347686ed10d53ce51bc67057df8b13
Hackbright Student <user@mini-003.(none)>
2013-10-01 16:39:29 -0700

    Markov.py is a working markov chain text generator. Creates a snippet of 200 characters based on input text

--- markov.py
+++ markov.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import sys
+import random
+import string
 
 # read in a text file give from the command line
 def read_in_file():
@@ -8,7 +10,7 @@ def read_in_file():
     script, filename = args
     f = open(filename)
     corpus = f.read()
-    # corpus = f.readline()
+    f.close()
 
     return corpus
 
@@ -18,22 +20,22 @@ def print_dict(chain):
         print key,value
 
 
-def make_uni_grams(corpus):
-    # create a dict with a value = word and key = list of words following it
-    chain = {}
+# def make_uni_grams(corpus):
+#     # create a dict with a value = word and key = list of words following it
+#     chain = {}
 
-    # goes through list of words in corpos minus last one
-    for i in range(len(corpus)-1):
-        word = corpus[i]
-        next_word = corpus[i+1]
+#     # goes through list of words in corpos minus last one
+#     for i in range(len(corpus)-1):
+#         word = corpus[i]
+#         next_word = corpus[i+1]
 
-        # adds following word to list of uni-grams
-        if word in chain:
-            chain[word].append(next_word)
-        else:
-            chain[word] = [next_word]
+#         # adds following word to list of uni-grams
+#         if word in chain:
+#             chain[word].append(next_word)
+#         else:
+#             chain[word] = [next_word]
 
-    return chain
+#     return chain
 
 
 
@@ -43,11 +45,12 @@ def make_chains(corpus):
 
     # convert string corpus text into list of words without spaces
     # corpus = corpus.replace(',',' ').replace('?',' ').split()
-    corpus = corpus.replace(',',' ').replace('?',' ').replace('\t','').split(" ")
-    print corpus
+    # corpus = corpus.replace(',',' ').replace('?',' ').replace('\t','').split(" ")
+    corpus = corpus.split()
+    # print corpus
 
     # create a dict with a value = word and key = list of words following it
-    chain = {}
+    chains = {}
 
     for i in range(len(corpus)-2):
         first_word = corpus[i]
@@ -57,26 +60,51 @@ def make_chains(corpus):
         key = (first_word,second_word)
 
         # adds the next word to the tuple key
-        if key in chain:
-            chain[key].append(next_word)
+        if key in chains:
+            chains[key].append(next_word)
         else:
-            chain[key] = [next_word]
+            chains[key] = [next_word]
 
-    print_dict(chain)
+    # print_dict(chains)
 
-    return {}
+    return chains
 
-def make_text(chains):
+
+def make_text(chain):
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
-    return "Here's some random text."
+
+    starting_point = random.choice(chain.keys())
+    first_char = starting_point[0][0][0]
+
+    while first_char in string.ascii_lowercase or first_char in string.punctuation:
+        starting_point = random.choice(chain.keys())
+        first_char = starting_point[0][0][0]
+
+    sentence = ""
+
+    while len(sentence) < 200:
+        first_word = starting_point[0]
+        second_word = starting_point[1]
+        third_word = random.choice(chain[starting_point])
+        sentence += first_word + " " 
+
+        starting_point = (second_word,third_word)
+
+        if starting_point not in chain.keys():
+            sentence += second_word + " " + third_word + " "
+            break
+
+    return sentence
+
 
 def main():
     corpus = read_in_file()
     
     chain_dict = make_chains(corpus)
-    # random_text = make_text(chain_dict)
-    # print random_text
+    random_text = make_text(chain_dict)
+    print random_text
+
 
 if __name__ == "__main__":
     main()
